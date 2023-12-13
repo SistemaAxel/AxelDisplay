8f0a37dae470919fc91f4b75b369f7c957b2afdb
naielv <109038805+naielv@users.noreply.github.com>
2023-12-13 20:16:12 +0100

    1234

--- update.sh
+++ update.sh
@@ -4,5 +4,8 @@ rm /home/pi/axeldisplay.py
 echo Downloading Code
 wget -O /home/pi/axeldisplay.py https://raw.githubusercontent.com/SistemaAxel/AxelDisplay/main/src/axeldisplay.py 
 
+echo Restarting Code
+sudo systemctl restart axeldisplay
+
 echo ¡Done!
 echo The HTTP Port is 8314
